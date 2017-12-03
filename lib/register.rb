7080b50faa021de7eefba3d520731adf6943999f
Sivan Patel <sivan.patel@googlemail.com>
2017-12-03 22:50:09 +0000

    a somewhat working solution

---
+++ lib/register.rb
@@ -0,0 +1,104 @@
+require_relative 'products'
+require 'pry'
+
+class Register
+
+  BOGOF_PRODUCTS = %w(chocolate)
+  HAPPY_HOUR_PRODUCTS = %w(beer soda)
+  MEAL_DEAL_PRODUCTS = %w(sandwich soda chocolate)
+  PRODUCTS = Products::PRODUCTS
+
+  def total_price
+    apply_meal_deal_discount if meal_deal_conditions_met
+    apply_bogof_discount if bogof_conditions_met
+    apply_happy_hour_rule if happy_hour?
+    apply_beer_discount if beer_condition_met
+    tally_remaining_products
+    @total_price
+  end
+
+  def initialize(cart)
+    @products = cart.products
+    @total_price = 0
+  end
+
+  private
+
+  def happy_hour?
+    current_time = Time.now
+    (current_time.hour >= 17) && (current_time.hour < 19)
+  end
+
+  def apply_happy_hour_rule
+    soda_quantity = find_quantity_of_product_in_cart("soda")
+    beer_quantity = find_quantity_of_product_in_cart("beer")
+    discounted_price = (beer_quantity * find_product_price("beer") + soda_quantity * find_product_price("soda")) / 2
+    @total_price += discounted_price
+    update_product_quantity("soda")
+    update_product_quantity("beer")
+  end
+
+  def bogof_conditions_met
+    products = []
+    BOGOF_PRODUCTS.each do |prod|
+      products << @products.find { |p| p[:product] == prod }
+    end
+    true unless products.empty? || !products.any?
+  end
+
+  def apply_bogof_discount
+    BOGOF_PRODUCTS.each do |product|
+      product_price = find_product_price(product)
+      product_quantity = find_quantity_of_product_in_cart(product)
+      @total_price += product_quantity % 2 == 0 ? (product_price * product_quantity)/2 : (product_price * (product_quantity / 2) + product_price)
+      update_product_quantity(product)
+    end
+  end
+
+  def meal_deal_conditions_met
+    products = []
+    MEAL_DEAL_PRODUCTS.each do |prod|
+      products << @products.find { |p| p[:product] == prod }
+    end
+    products.compact.length == MEAL_DEAL_PRODUCTS.length
+  end
+
+  def apply_meal_deal_discount
+    minimum_amount_of_products = [find_quantity_of_product_in_cart("soda"), find_quantity_of_product_in_cart("sandwich"), find_quantity_of_product_in_cart("chocolate")].min
+    @total_price += find_quantity_of_product_in_cart("soda") * find_product_price("soda"); update_product_quantity("soda")
+    @total_price += find_quantity_of_product_in_cart("sandwich") * find_product_price("sandwich"); update_product_quantity("sandwich")
+    @total_price += (find_quantity_of_product_in_cart("chocolate") - minimum_amount_of_products) * find_product_price("chocolate"); update_product_quantity("chocolate")
+  end
+
+  def beer_condition_met
+    find_quantity_of_product_in_cart("beer") >= 3
+  end
+
+  def apply_beer_discount
+    number_of_beers_in_cart = find_quantity_of_product_in_cart("beer")
+    beer_price = find_product_price("beer")
+    @total_price += number_of_beers_in_cart % 3 == 0 ? (beer_price * number_of_beers_in_cart) * 0.9 :
+        (beer_price * (number_of_beers_in_cart / 3) + (beer_price * (number_of_beers_in_cart % 3)))
+    update_product_quantity("beer")
+  end
+
+  def tally_remaining_products
+    @products.each do |prod|
+      @total_price += find_product_price(prod[:product]) * find_quantity_of_product_in_cart(prod[:product])
+    end
+  end
+
+  def update_product_quantity(product)
+    if @products.find { |prod| prod[:product] == product }
+      @products.find { |prod| prod[:product] == product }[:quantity] = 0
+    end
+  end
+
+  def find_product_price(product)
+    PRODUCTS.find { |prod| prod[:name] == product }[:price]
+  end
+
+  def find_quantity_of_product_in_cart(product)
+    @products.find { |prod| prod[:product] == product }&.[](:quantity) || 0
+  end
+end
--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -1,1 +1,36 @@
-require '../lib/register'
+require './lib/register'
+
+describe Register do
+
+  let(:cart) { double("cart") }
+
+  it 'returns the price with 1 product in the cart' do
+    allow(cart).to receive(:products) { [{ product: "beer", quantity: 1 }] }
+    expect(Register.new(cart).total_price).to eq 1.5
+  end
+
+  it 'charges for only 1 chocolate when 2 are added' do
+    allow(cart).to receive(:products) { [{ product: "chocolate", quantity: 2 }] }
+    expect(Register.new(cart).total_price).to eq 3.10
+  end
+  
+  it 'discounts 10% for every batch of 3 beers bought' do
+    allow(cart).to receive(:products) { [{ product: "beer", quantity: 3 }] }
+    expect(Register.new(cart).total_price).to eq 4.05
+  end
+
+  it 'gives a free chocolate if soda and sandwich are in the cart' do
+    allow(cart).to receive(:products) { [{ product: "soda", quantity: 1 },
+                                         { product: "sandwich", quantity: 1 },
+                                         { product: "chocolate", quantity: 1  }] }
+    expect(Register.new(cart).total_price).to eq 6.50
+  end
+
+  it 'gives 50% off beer and soda when in happy hour' do
+    allow(cart).to receive(:products) { [{ product: "beer", quantity: 1 },
+                                         { product: "soda", quantity: 1 }] }
+    allow(Time).to receive(:now) { Time.new(2017, 12, 03, 17, 15, 00) }
+    expect(Register.new(cart).total_price).to eq 2.00
+  end
+
+end
