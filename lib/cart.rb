07436c37b0efd2bc76b6f853c85966b481f334bb
Sivan Patel <sivan.patel@googlemail.com>
2017-12-03 18:41:19 +0000

    can add different quantities of products

--- lib/cart.rb
+++ lib/cart.rb
@@ -10,21 +10,29 @@ class Cart
     @products
   end
 
-  def add_product(product)
+  def add_product_to_cart(product)
     if product_recognized?(product)
-      @products << product
+      add_product(product)
     else
       raise "Unrecognized product: #{product}"
     end
   end
 
-  def remove_product(product)
-    @products.delete(product) {"product not in cart"}
+  def remove_product_from_cart(product)
+    @products.delete_if { |prod| prod[:product] == product }
   end
 
   private
 
   def product_recognized?(product)
     Products::PRODUCTS.find { |prod| prod[:name] == product }
+  end
+
+  def add_product(product)
+    if @products.find { |prod| prod[:product] == product }
+      @products.find { |prod| prod[:product] == product }[:quantity] += 1
+    else
+      @products << { product: product, quantity: 1 }
+    end
   end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,35 +1,31 @@
 require './lib/cart.rb'
 
 describe Cart do
+  let(:cart) { Cart.new }
 
   it 'starts with an empty cart' do
-    cart = Cart.new
     expect(cart.products).to be_empty
   end
 
   it 'can add products to the cart' do
-    cart = Cart.new
-    cart.add_product("beer")
-    expect(cart.products).to eq ["beer"]
+    cart.add_product_to_cart("beer")
+    expect(cart.products).to eq [{ product: "beer", quantity: 1}]
   end
 
+  it 'can add multiple of the same product to the cart' do
+    cart.add_product_to_cart("beer")
+    cart.add_product_to_cart("beer")
+    expect(cart.products).to eq [{ product: "beer", quantity: 2 }]
+  end
   it 'will not add an unrecognized product to the cart' do
-    cart = Cart.new
-    expect { cart.add_product("sausages") }
+    expect { cart.add_product_to_cart("sausages") }
       .to raise_error 'Unrecognized product: sausages'
     expect(cart.products).to be_empty
   end
 
   it 'can remove products from the cart' do
-    cart = Cart.new
-    cart.add_product("beer")
-    expect(cart.products).to eq ["beer"]
-    cart.remove_product("beer")
+    cart.add_product_to_cart("beer")
+    cart.remove_product_from_cart("beer")
     expect(cart.products).to be_empty
-  end
-
-  it 'will raise an error if trying to delete an item not in cart' do
-    cart = Cart.new
-    expect(cart.remove_product("beer")).to eq "product not in cart"
   end
 end
